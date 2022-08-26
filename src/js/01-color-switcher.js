86aabbae0ffc20a06fff478b6b3a8aeab86b7dbb
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-08-26 23:09:21 +0300

    Script for task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+  const btnStartRef = document.querySelector("button[data-start]");
+  const btnStopRef = document.querySelector("button[data-stop]");
+  const bodyRef = document.querySelector('body');
+
+  let intervalId = null;
+
+  btnStartRef.addEventListener('click', onStartBtn);
+  btnStopRef.addEventListener('click', onStopBtn);
+
+  btnStopRef.setAttribute('disabled','disabled')
+
+  function onStartBtn(){
+    btnStartRef.setAttribute('disabled','disabled');
+    btnStopRef.removeAttribute('disabled');
+
+    intervalId = setInterval(changeBgColors, 1000);
+  
+  }
+
+function changeBgColors(){
+  let rendomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = rendomColor;
+  console.log(bodyRef.style.backgroundColor);
+}
+
+  function onStopBtn(){
+    btnStopRef.setAttribute('disabled','disabled');
+    btnStartRef.removeAttribute('disabled');
+    clearInterval(intervalId);
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
